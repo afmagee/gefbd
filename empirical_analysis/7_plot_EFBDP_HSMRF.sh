0a98e59f84a053d57f734becfc6f8ae049ee95d7
Sebastian Hoehna <sebastian.hoehna@gmail.com>
2022-06-12 17:32:50 +0200

    parallel plotting

--- empirical_analysis/7_plot_EFBDP_HSMRF.sh
+++ empirical_analysis/7_plot_EFBDP_HSMRF.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-#SBATCH --job-name=BDSTP_emp_plot
-#SBATCH --output=BDSTP_emp_plot.log
-#SBATCH --error=BDSTP_emp_plot.err
+#Rscript src/plot_HSMRFBDP.R empirical_analysis/output empirical_analysis/figures
+#Rscript src/plot_HSMRFBDP.R empirical_analysis/output_treatment empirical_analysis/figures_treatment
+
+N_CORES=4
+JOB_DIR="empirical_analysis/jobs_HSMRF_plot"
+LOG_DIR="empirical_analysis/logs_HSMRF_plot"
+
+for me_prior in "0.0" "0.1" "0.5" "1.0" "2.0" "5.0";
+do
+
+    for uncertainty in "none" "tip" "node" "both";
+    do
+
+        for ds in "Wilberg" "Stubbs";
+        do
+
+            echo "#!/bin/bash
+#SBATCH --job-name=BDSTP_emp_plot_${ds}_${uncertainty}_${me_prior}
+#SBATCH --output=BDSTP_emp_plot_${ds}_${uncertainty}_${me_prior}.log
+#SBATCH --error=BDSTP_emp_plot_${ds}_${uncertainty}_${me_prior}.err
 #SBATCH --ntasks=2
 #SBATCH --nodes=1
 #SBATCH --mem=32G
@@ -10,19 +27,12 @@
 #SBATCH --mail-user sebastian.hoehna@gmail.com
 #SBATCH --mail-type=ALL
 
-
-#Rscript src/plot_HSMRFBDP.R empirical_analysis/output empirical_analysis/figures
-#Rscript src/plot_HSMRFBDP.R empirical_analysis/output_treatment empirical_analysis/figures_treatment
-
 module load R
 
-for uncertainty in "none" "tip" "node" "both";
-do
+Rscript src/plot_Wilberg.R empirical_analysis/output_${ds}_${uncertainty} empirical_analysis/figures_${ds}_${uncertainty} HSMRFBDP ${ds} ${me_prior}" > ${JOB_DIR}/${ds}_${uncertainty}_${me_prior}.sh
+            sbatch ${JOB_DIR}/${ds}_${uncertainty}_${me_prior}.sh
 
-    for ds in "Wilberg" "Stubbs";
-    do
-
-	    Rscript src/plot_Wilberg.R empirical_analysis/output_${ds}_${uncertainty} empirical_analysis/figures_${ds}_${uncertainty} HSMRFBDP ${ds}
+        done
 
 	done
 
--- src/plot_Wilberg.R
+++ src/plot_Wilberg.R
@@ -8,6 +8,7 @@ OUTPUT_DIR  = args[1]
 FIGURES_DIR = args[2]
 MODEL       = args[3]
 ds.names    = args[4]
+this.ME     = args[5]
 #OUTPUT_DIR  = "empirical_analysis/output_tip_age"
 #FIGURES_DIR = "figures_tip_age"
 
@@ -23,40 +24,35 @@ grid.times <- computeGridEndTimes(100,243.5)
 #ds.names <- "Stubs2021_cal3_1"
 #ds.names <- "gavia_mol_minus_thoracosaurs.equal.tre"
 
-ME.priors <- c("0","0.1","0.5","1","2","5")
+#ME.priors <- c("0","0.1","0.5","1","2","5")
 #ME.priors <- c("0","0.5")
 
 ## Create figures directory
 dir.create(FIGURES_DIR, showWarnings=FALSE)
 
-for (i in 1:length(ME.priors)) {
-  this.ME <- ME.priors[i]
+cat(this.ME,"\n")
 
-  cat(this.ME,"\n")
+ds = ds.names
 
-  for (ds in ds.names) {
+cat("\t",ds,"\n")
 
-    cat("\t",ds,"\n")
+posterior <- getConcatenatedLogFile(paste0(OUTPUT_DIR,"/", MODEL,"_ME_prior_",this.ME,"_",ds,""))
 
-    posterior <- getConcatenatedLogFile(paste0(OUTPUT_DIR,"/", MODEL,"_ME_prior_",this.ME,"_",ds,""))
+pdf(paste0(FIGURES_DIR, "/continuous_parameters_ME_prior_",this.ME,"_",ds,"_",MODEL,".pdf"),width=8,height=2)
+  plotContinuousParameters(rb.posterior = posterior,
+                           interval.times = grid.times,
+                           parameter.names = c("speciation","extinction","fossilization"),
+                           ci.width = 0.9)
+dev.off()
 
-    pdf(paste0(FIGURES_DIR, "/continuous_parameters_ME_prior_",this.ME,"_",ds,"_",MODEL,".pdf"),width=8,height=2)
-      plotContinuousParameters(rb.posterior = posterior,
-                               interval.times = grid.times,
-                               parameter.names = c("speciation","extinction","fossilization"),
-                               ci.width = 0.9)
-    dev.off()
-
-    if (this.ME != "0") {
-      pdf(paste0(FIGURES_DIR, "/ME_prior_",this.ME,"_",ds,"_",MODEL,".pdf"),width=4,height=3)
-        plotMEBF(rb.posterior = posterior,
-                 interval.times = grid.times,
-                 parameter.name = "mass_extinction_probabilities",
-                 prior.probability = as.numeric(this.ME)/99)
-      dev.off()
-    }
-    rm(posterior)
-  }
+if (this.ME != "0") {
+  pdf(paste0(FIGURES_DIR, "/ME_prior_",this.ME,"_",ds,"_",MODEL,".pdf"),width=4,height=3)
+    plotMEBF(rb.posterior = posterior,
+             interval.times = grid.times,
+             parameter.name = "mass_extinction_probabilities",
+             prior.probability = as.numeric(this.ME)/99)
+  dev.off()
+}
+rm(posterior)
 
 cat("\n")
-}
